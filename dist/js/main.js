f7f4c97f6a8290b6c05dc7af1eeb9523464e3a25
jfperez5 <65435098+jfperez5@users.noreply.github.com>
2025-05-19 23:25:32 -0600

    section menu terminada

--- dist/js/main.js
+++ dist/js/main.js
@@ -16,3 +16,54 @@ hamburger.addEventListener("click", ()=>{
 closeIcon.addEventListener("click", ()=>{
     navMenu.classList.add('hidden')
 })
+
+/*~~~~~~~~~~~~~~~~~~~~ TABS ~~~~~~~~~~~~~~~~~~~~*/
+const tabs = document.querySelectorAll(".tabs_wrap ul li");
+const all = document.querySelectorAll(".item_wrap");
+const foods = document.querySelectorAll(".food");
+const snacks = document.querySelectorAll(".snack");
+const beverages = document.querySelectorAll(".beverage");
+
+tabs.forEach(tab => {
+    tab.addEventListener("click", ()=> {
+        tabs.forEach(tab => {
+            tab.classList.remove("active")
+        })
+
+        tab.classList.add('active')
+
+        const tabval = tab.getAttribute('data-tabs')
+        console.log(all)
+
+        all.forEach(item => {
+            item.style.display = 'none'
+        })
+
+        if(tabval == 'food'){
+             foods.forEach(item => {
+                    item.style.display = 'block'
+             })
+       } else if(tabval == 'snack'){
+             snacks.forEach(item => {
+                item.style.display = 'block'
+             })
+       } else if(tabval == 'beverage'){
+             beverages.forEach(item => {
+                item.style.display = 'block'
+            })
+       }else{
+            all.forEach(item => {
+                item.style.display = 'block'
+            })
+       }
+
+    })
+})
+
+
+
+/*~~~~~~~~~~~~~~~~ DARK LIGHT THEME ~~~~~~~~~~~~~~~*/
+/*~~~~~~~~~~~~~~~~ SHOW SCROLL UP ~~~~~~~~~~~~~*/
+/*~~~~~~~~~~~~~ CHANGE BACKGROUND HEADER ~~~~~~~~~~~~~*/
+/*~~~~~~~~~~~~~ SCROLL SECTIONS ACTIVE LINK ~~~~~~~~~~~~*/
+/*~~~~~~~~~~~~ SCROLL REVEAL ANIMATION ~~~~~~~~~~~~*/
